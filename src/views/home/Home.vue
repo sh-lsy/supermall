e5053f7042e720f131773b53fae711e458c23929
SH <673545134@qq.com>
2020-04-13 19:18:12 +0800

    首页内容添加

--- src/views/home/Home.vue
+++ src/views/home/Home.vue
@@ -1,13 +1,45 @@
 <template>
-  <h2>首页</h2>
+  <div id="home">
+    <nav-bar class="home-nav"><div slot="center">购物街</div></nav-bar>
+    <home-swiper :banners="banners"></home-swiper>
+    <recommend-view :recommends="recommend"></recommend-view>
+  </div>
 </template>
 
 <script>
-export default {
-  name:'Home'
-}
+  import NavBar from 'components/common/navbar/NavBar'
+  import HomeSwiper from './childComps/HomeSwiper'
+  import RecommendView from './childComps/RecommendView';
+
+  import {getHomeMultidata} from 'network/home'
+ 
+
+  export default {
+    name:'Home',
+    components:{
+      NavBar,
+      HomeSwiper,
+      RecommendView
+    },
+    data(){
+      return{
+        banners:[],
+        recommend:[]
+      }
+    },
+    created(){
+      getHomeMultidata().then(res =>{
+        console.log(res);
+        this.banners = res.data.banner.list;
+        this.recommend = res.data.recommend.list;
+      })
+    }
+  }
 </script>
 
 <style>
-
+  .home-nav{
+    background-color:var(--color-tint);
+    color: #fff;
+  }
 </style>
---
+++ src/views/home/childComps/HomeSwiper.vue
@@ -0,0 +1,31 @@
+<template>
+  <swiper>
+      <swiper-item  v-for="item in banners" :key="item.image">
+        <a :href="item.link">
+          <img :src="item.image" alt="">
+        </a>
+      </swiper-item>
+    </swiper>
+</template>
+
+<script>
+ import {Swiper, SwiperItem} from 'components/common/swiper'
+  export default {
+    props:{
+      banners:{
+        type:Array,
+        default(){
+          return []
+        }
+      }
+    },
+    components:{
+      Swiper, 
+      SwiperItem
+    },
+  }
+</script>
+
+<style>
+
+</style>
